cf2b57124f63643e31bef6b27a0284468c6b40fb
jakuski <jakubstanieski@gmail.com>
2022-05-25 12:25:58 +0100

    fix tracking scripts

--- pages/api/go-redirect-service/[...name].js
+++ pages/api/go-redirect-service/[...name].js
@@ -1,8 +1,5 @@
 import Data from "./data.json";
-
-console.log(Data);
-
-
+import axios from "axios";
 
 export default function handler(req, res) {
 	const name = req.query.name.join("/").replace(".html", ""); // < legacy website would accept .html redirects
@@ -11,5 +8,35 @@ export default function handler(req, res) {
 
 	if (!redirectUrl) return res.status(404).end("Not Found");
 
-	return res.redirect(301, redirectUrl);
+	res.redirect(302, redirectUrl);
+
+	if (req.query.s) {
+		const publicToken = "80012d85736117b337fbb36f2ff51d68",
+			sources = {
+				i: "Instagram",
+				l: "LinkedIn",
+				t: "Twitter",
+				p: "Portfolio",
+				v: "Vimeo"
+			};
+
+		const sourceParam = req.query.s;
+
+		const sourcePrettified = sources[sourceParam];
+
+		axios({
+			url: "https://api.logsnag.com/v1/log",
+			method: "post",
+			headers: {
+				Authorization: "Bearer " + publicToken,
+			},
+			data: {
+				project: "jakub-studio-website",
+				channel: "redirect-source-tracking",
+				event: `Visitor from ${sourcePrettified || sourceParam} to ${req.query.name}`,
+				description: "Original Parameter: " + sourceParam + "\nDate: " + new Date().toUTCString(),
+				icon: "✈",
+			}
+		}).catch(console.error);
+	}
 }
