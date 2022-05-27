40a7910a1cefdd0b68495403f3c2c80388b3b230
jakuski <jakubstanieski@gmail.com>
2022-05-27 14:35:58 +0100

    update footer to include vimeo

--- components/footer.js
+++ components/footer.js
@@ -10,15 +10,16 @@ const SocialLink = props => {
 
 export default function Footer({ children }) {
 	return (
-		<footer className="font-sans text-xs flex flex-col items-center pb-3 mt-4">
-			<div className="italic mb-3">Slava Ukraini.</div>
-			<div className="mb-3 flex gap-2">
+		<footer className="font-sans text-sm flex flex-col items-center mt-4 gap-3">
+			
+			<div className="flex gap-2">
 				<SocialLink href="/go/instagram">Instagram</SocialLink>
 				<SocialLink href="/go/twitter">Twitter</SocialLink>
 				<SocialLink href="/go/linkedin">LinkedIn</SocialLink>
+				<SocialLink href="/go/vimeo">Vimeo</SocialLink>
 			</div>
 			{/* <div>Powered by <a href="https://vercel.com/">Vercel</a> and available on <a href={pkg.repository.url}>GitHub</a>.</div> */}
-			<div>Copyright © {new Date().getFullYear()} Jakub Staniszewski</div>
+			<div><span className="italic">Slava Ukraini.</span>Copyright © {new Date().getFullYear()} Jakub Staniszewski</div>
 		</footer>
 	);
 }
