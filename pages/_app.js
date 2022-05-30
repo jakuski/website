f29b2bedfe8628323264353cd4f10e67904b6aef
Jakub S <34782021+jakuski@users.noreply.github.com>
2022-05-31 00:33:24 +0100

    add defer attrib to cloudflare analytics

--- pages/_app.js
+++ pages/_app.js
@@ -19,6 +19,7 @@ function MyApp({ Component, pageProps }) {
 			strategy="lazyOnload"
 		/>
 		<Script
+			defer
 			src="https://static.cloudflareinsights.com/beacon.min.js"
 			data-cf-beacon='{"token": "d43da5b3e6f048098e1b3b72912373bf"}'
 		/>
