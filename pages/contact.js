bae83668204b28d855cb9e56494a9b127563c275
Jakub S <34782021+jakuski@users.noreply.github.com>
2022-05-31 02:20:03 +0100

    change to ayup address

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,7 @@
 import Post from "../components/Post";
 
 export default function Contact () {
-	return <Post title="Ay-up.">If you would like to get in touch with me, please send me an email at jakub@jakub.studio.
+	return <Post title="Ay-up.">If you would like to get in touch with me, please send me an email at ayup@jakub.studio.
 		<br/><br/>
 	I aim to reply within two working days however if I don&apos;t get back to you within then, please be patient as your email may have landed in my spam/junk folder which I check less frequently. </Post>;
 }
