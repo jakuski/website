2c532526a705bb8b2b107d07731cae443526b938
Jakub S <34782021+jakuski@users.noreply.github.com>
2022-06-03 16:06:34 +0100

    update tracking

--- pages/api/go-redirect-service/[...name].js
+++ pages/api/go-redirect-service/[...name].js
@@ -8,35 +8,37 @@ export default function handler(req, res) {
 
 	if (!redirectUrl) return res.status(404).end("Not Found");
 
+	try {
+		if (req.query.s) {
+			const publicToken = "80012d85736117b337fbb36f2ff51d68",
+				sources = {
+					i: "Instagram",
+					l: "LinkedIn",
+					t: "Twitter",
+					p: "Portfolio",
+					v: "Vimeo"
+				};
+	
+			const sourceParam = req.query.s;
+	
+			const sourcePrettified = sources[sourceParam];
+	
+			axios({
+				url: "https://api.logsnag.com/v1/log",
+				method: "post",
+				headers: {
+					Authorization: "Bearer " + publicToken,
+				},
+				data: {
+					project: "jakub-studio-website",
+					channel: "redirect-source-tracking",
+					event: `Visitor from ${sourcePrettified || sourceParam} to ${req.query.name}`,
+					description: "Original Parameter: " + sourceParam + "\nDate: " + new Date().toUTCString(),
+					icon: "✈",
+				}
+			}).catch(console.error);
+		}
+	} catch (e) { console.error(e); }
+
 	res.redirect(302, redirectUrl);
-
-	if (req.query.s) {
-		const publicToken = "80012d85736117b337fbb36f2ff51d68",
-			sources = {
-				i: "Instagram",
-				l: "LinkedIn",
-				t: "Twitter",
-				p: "Portfolio",
-				v: "Vimeo"
-			};
-
-		const sourceParam = req.query.s;
-
-		const sourcePrettified = sources[sourceParam];
-
-		axios({
-			url: "https://api.logsnag.com/v1/log",
-			method: "post",
-			headers: {
-				Authorization: "Bearer " + publicToken,
-			},
-			data: {
-				project: "jakub-studio-website",
-				channel: "redirect-source-tracking",
-				event: `Visitor from ${sourcePrettified || sourceParam} to ${req.query.name}`,
-				description: "Original Parameter: " + sourceParam + "\nDate: " + new Date().toUTCString(),
-				icon: "✈",
-			}
-		}).catch(console.error);
-	}
 }
