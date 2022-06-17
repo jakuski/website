acd38f74192527aa1f20c7f260d30b0f75593753
Jakub S <34782021+jakuski@users.noreply.github.com>
2022-06-17 22:17:52 +0100

    add next availbility

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,9 @@
 import Post from "../components/Post";
 
 export default function Contact () {
-	return <Post title="Ay-up.">If you would like to get in touch with me, please send me an email at ayup@jakub.studio.
+	return <Post title="Ay-up.">
+		<b>Next available:</b> September 2022 <br/><br/>
+		If you would like to get in touch with me, please send me an email at ayup@jakub.studio.
 		<br/><br/>
 	I aim to reply within two working days however if I don&apos;t get back to you within then, please be patient as your email may have landed in my spam/junk folder which I check less frequently. </Post>;
 }
