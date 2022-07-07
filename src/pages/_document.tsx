03e31c29ae783f3de6968e942e8ea63af44fa657
Jakub S <34782021+jakuski@users.noreply.github.com>
2022-07-07 15:38:39 +0100

    spacing

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,8 +28,7 @@ class SiteDocument extends Document {
 				<body>
 					<noscript>
 						For full functionality of this site it is necessary to enable JavaScript.
-						Here are the
-						<a href="https://www.enable-javascript.com/">instructions how to enable JavaScript in your web browser</a>.
+						Here are the <a href="https://www.enable-javascript.com/">instructions how to enable JavaScript in your web browser</a>.
 					</noscript>
 					<Main />
 					<NextScript />
