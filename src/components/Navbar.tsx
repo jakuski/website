dd24e0e6164bc95a19660b1e6703af5c9d73e7dd
Jakub S <34782021+jakuski@users.noreply.github.com>
2022-10-13 11:49:30 +0100

    added dark colour scheme and navbar improvements

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,19 +5,23 @@ import LogoIcon from "./icons/Logo";
 const NavbarLink: React.FC<React.PropsWithChildren<{
 	href: string;
 }>> = props => {
-	return <li>
+	return <li className="px-2 last:pl-2 last:pr-0">
 		<Link href={props.href}>
-			<a className="text-md font-semibold flex items-center px-2 min-h-44 lowercase last:pl-2 last:pr-0">{props.children}</a>
+			<a className="flex items-center min-h-44">{props.children}</a>
 		</Link>
 	</li>;
+};
+
+const SkipToMainButton: React.FC = () => {
+	return <a href="#main-content" className="absolute left-0 top-4 bg-black text-brand pointer-events-none shadow-md z-20 py-4 px-6 -translate-x-full transition-transform duration-75 focus:pointer-events-auto focus:-translate-x-0">
+		Skip to main content
+	</a>;
 };
 
 export default function Navbar() {
 	return (
 		<>
-			<a href="#main-content" className="absolute left-0 top-4 bg-black text-brand pointer-events-none shadow-md z-20 py-4 px-6 -translate-x-full transition-transform duration-75 focus:pointer-events-auto focus:-translate-x-0">
-				Skip to main content
-			</a>
+			<SkipToMainButton />
 
 			<header className="p-4 md:p-6 fixed w-full flex justify-between items-center select-none z-10 print:hidden">
 				<Link href={Home.href}>
@@ -26,7 +30,7 @@ export default function Navbar() {
 					</a>
 				</Link>
 
-				<nav className="font-serif">
+				<nav className="font-serif text-md font-semibold lowercase">
 					<ul className="flex gap-4">
 						{navbarRoutes.map(route => (
 							<NavbarLink
