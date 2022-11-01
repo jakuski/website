b06f3abcc84c1a4705319f9ecc8074e457f16a16
Jakub S <34782021+jakuski@users.noreply.github.com>
2022-11-01 00:23:31 +0000

    sync

--- src/components/StorageConsent/Banner.tsx
+++ src/components/StorageConsent/Banner.tsx
@@ -1,8 +1,9 @@
 import { CookiePolicy, Legal, Privacy, Route } from "@/routes";
 import NextLink from "next/link";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
 import Button from "./Button";
 import CookieClickerEasterEgg from "./CookieClickerEasterEgg";
+import c from "clsx";
 
 const Link: React.FC<PropsWithChildren<{
 	href: Route
@@ -17,11 +18,18 @@ const Link: React.FC<PropsWithChildren<{
 };
 
 const StorageConsentBanner = () => {
+	const [visible, setVisible] = useState(false);
+
+	const containerClassName = c("bg-stone-800/90 backdrop-blur-lg text-stone-200 text-center py-4 px-6 max-w-3xl mx-auto rounded-md shadow-lg pointer-events-auto",
+		"transition-transform translate ease duration-150 transform-gpu", {
+			["translate-y-36"]: !visible
+		});
+
 	return (
-		<div className="fixed bottom-2 left-0 right-0 mx-2 z-50">
-			<div className="bg-stone-800/90 backdrop-blur-lg text-stone-200 text-center py-4 px-6 max-w-3xl mx-auto rounded-md shadow-lg">
+		<div className="fixed bottom-2 left-0 right-0 mx-2 z-50 pointer-events-none">
+			<div className={containerClassName} aria-hidden={!visible}>
 				<div className="flex flex-col sm:flex-row gap-4 md:gap-6 justify-between">
-					<div className="flex flex-row gap-4">
+					<div className="flex flex-row gap-6">
 						<CookieClickerEasterEgg />
 						<div className="flex flex-col gap-1 items-start justify-center">
 							<div className="text-xs text-left flex gap-1 flex-col">
