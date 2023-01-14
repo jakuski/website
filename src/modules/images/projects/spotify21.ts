4479907e52344239eba4e4a9411a8c005cfa758e
Jakub S <34782021+jakub-studio@users.noreply.github.com>
2023-01-14 19:48:05 +0000

    add postcodes_playlists img

--- src/modules/images/projects/spotify21.ts
+++ src/modules/images/projects/spotify21.ts
@@ -3,6 +3,7 @@ import { ImageObject } from "@/modules/images";
 import imageCover from "@/content/_images/projects/uni-y2-spotify/project_cover.jpg";
 import imagePostCodesOnboarding from "@/content/_images/projects/uni-y2-spotify/postcodes_onboarding.jpg";
 import imagePostCodesExploration from "@/content/_images/projects/uni-y2-spotify/postcodes_exploration.jpg";
+import imagePostCodesPlaylists from "@/content/_images/projects/uni-y2-spotify/postcodes_playlists.jpg";
 
 const images: ImageObject = {
 	"p/spotify21/cover": {
@@ -16,8 +17,11 @@ const images: ImageObject = {
 	"p/spotify21/postcodes_exploration": {
 		src: imagePostCodesExploration,
 		alt: "3 mobile devices with the Spotify app open, showing the exploration phase of the conceptual 'Post Codes' feature"
+	},
+	"p/spotify21/postcodes_playlists": {
+		src: imagePostCodesPlaylists,
+		alt: "3 mobile devices with the Spotify app open, showing the playlists phase of the conceptual 'Post Codes' feature"
 	}
-
 };
 
 export default images;
