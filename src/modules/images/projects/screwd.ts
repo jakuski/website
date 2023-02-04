65f8be53364b605a86e008a29354442ba8a9228f
Jakub S <34782021+jakub-studio@users.noreply.github.com>
2023-02-04 15:31:48 +0000

    add image definitions for screwd

--- src/modules/images/projects/screwd.ts
+++ src/modules/images/projects/screwd.ts
@@ -1,11 +1,36 @@
 import { ImageObject } from "@/modules/images";
 
 import imageMockup from "@/content/_images/projects/uni-y3-screwd/mockup.jpg";
+import imageMainMenu from "@/content/_images/projects/uni-y3-screwd/screwd_main_menus.jpg";
+import imageScrewScans from "@/content/_images/projects/uni-y3-screwd/screwd_screw_scans.jpg";
+import imageScrewScanResult from "@/content/_images/projects/uni-y3-screwd/screwd_screw_scan_result.jpg";
+import imageIos from "@/content/_images/projects/uni-y3-screwd/screwd_ios.jpg";
+import imageErrorState from "@/content/_images/projects/uni-y3-screwd/screwd_error_state.jpg";
 
 const images: ImageObject = {
 	"p/screwd/mockup_1": {
 		src: imageMockup,
 		alt: "Phones showcasing screwd's user interface"
+	},
+	"p/screwd/main_menu": {
+		src: imageMainMenu,
+		alt: "The main pages of the screwd app",
+	},
+	"p/screwd/screw_scans": {
+		src: imageScrewScans,
+		alt: "The scanning process of the screwd app",
+	},
+	"p/screwd/screw_scan_result": {
+		src: imageScrewScanResult,
+		alt: "The screw scan result page of the screwd app",
+	},
+	"p/screwd/ios": {
+		src: imageIos,
+		alt: "How the screwd app would look on iOS",
+	},
+	"p/screwd/error_state": {
+		src: imageErrorState,
+		alt: "The error state of the screwd app",
 	},
 };
 
