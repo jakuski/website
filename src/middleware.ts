c79033fa7fe388a0d9cb4976b37d3db1724bce1b
Jakub S <34782021+jakub-studio@users.noreply.github.com>
2023-02-18 23:27:32 +0000

    remove console.log

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,6 @@ import type { NextRequest, NextFetchEvent } from "next/server";
 import { recordSource } from "@/modules/source-query-tracker";
 
 export function middleware(req: NextRequest, event: NextFetchEvent) {
-	console.log("Middleware: ", req.nextUrl.pathname);
 	// Record ?s= query parameter
 	const sourceParam = req.nextUrl.searchParams.get("s");
 	if (sourceParam) {
