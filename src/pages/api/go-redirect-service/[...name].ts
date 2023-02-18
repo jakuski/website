e5c34451b389de6c3d4e97023541ea10e2f08b90
Jakub S <34782021+jakub-studio@users.noreply.github.com>
2023-02-18 20:05:39 +0000

    Update redirect API route to edge

--- src/pages/api/go-redirect-service/[...name].ts
+++ src/pages/api/go-redirect-service/[...name].ts
@@ -1,11 +1,33 @@
+import type { NextRequest } from "next/server";
+
 import Data from "./data.json";
-
-export default function handler(req, res) {
-	const name = req.query.name.join("/").replace(".html", ""); // < legacy website would accept .html redirects
-
-	const redirectUrl = Data.links[name];
-
-	if (!redirectUrl || Data.tempDisable[name]) return res.status(404).end();
-
-	res.redirect(302, redirectUrl);
+interface DataStructure {
+	links: { [key: string]: string };
+	tempDisable : { [key: string]: string };
 }
+
+// Update this if the file name ([...name].ts) changes
+const QUERY_NAME = "name";
+
+const notFoundConfig = { status: 404 };
+
+export default function handler(req: NextRequest) {
+	const requestedDestination = new URL(req.url).searchParams.getAll(QUERY_NAME).join("/");
+	if (!requestedDestination) return new Response(null, notFoundConfig);
+
+	const destination = (Data as DataStructure).links[requestedDestination];
+
+	// Check if the destination is disabled
+	if (!destination || (Data as DataStructure).tempDisable[requestedDestination]) return new Response(null, notFoundConfig);
+
+	return new Response(null, {
+		status: 302,
+		headers: {
+			Location: destination,
+		},
+	});
+}
+
+export const config = {
+	runtime: "edge",
+};
