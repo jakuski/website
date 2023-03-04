f431e04765c91cc6e56d6991a078c1dfcf853900
Jakub S <34782021+jakuski@users.noreply.github.com>
2023-03-04 00:38:48 +0000

    wip csp report route

--- src/pages/api/csp-report.ts
+++
@@ -1,9 +0,0 @@
-import type { NextRequest } from "next/server";
-
-export default function handler(req: NextRequest) {
-	return new Response(null);
-}
-
-export const config = {
-	runtime: "edge"
-};
