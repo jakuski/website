4d01624102c43b6d19656b54b0fe6416273f6a1f
Jakub <34782021+jakub-studio@users.noreply.github.com>
2023-05-14 20:18:30 +0100

    add tailwind class utility

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,11 @@
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+// credit/usage: https://twitter.com/shadcn/status/1614692419039105024
+export const c = (...inputs: ClassValue[]): string => {
+	return twMerge(clsx(inputs));
+};
+
 export const isProd = process.env.NODE_ENV === "production";
 export const isDev = !isProd;
 
