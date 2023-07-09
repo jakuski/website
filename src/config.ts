4f4dfa7432e0d0beb50ffa5b56d5cf047215c260
Jakub <34782021+jakub-studio@users.noreply.github.com>
2023-07-09 18:58:45 +0100

    feat: update eslint rules

--- src/config.ts
+++ src/config.ts
@@ -16,4 +16,4 @@ export const socialHandles = {
 // runs document.designMode = "on" when isDev (defined below) is true.
 export const useDocumentDesignModeDuringDev = false;
 
-export const recordSourceQueryMiddleware: boolean = true;
+export const recordSourceQueryMiddleware = true;
